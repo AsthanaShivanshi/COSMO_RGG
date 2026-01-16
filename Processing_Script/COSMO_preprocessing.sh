0a25dbf609da20f992f0a5a2454430f1ae716125
sasthana <shivanshi.asthana@unil.ch>
2026-01-16 16:22:11 +0100

    Changes

--- Processing_Script/COSMO_preprocessing.sh
+++ Processing_Script/COSMO_preprocessing.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=COSMO_preprocessing
 #SBATCH --chdir=/work/FAC/FGSE/IDYST/tbeucler/downscaling
-#SBATCH --output=sasthana/Downscaling/COSMO_RGG/logs/chron_split_observational_%A_%a.log
-#SBATCH --error=sasthana/Downscaling/COSMO_RGG/logs/chron_split_observational_%A_%a.log
+#SBATCH --output=sasthana/Downscaling/COSMO_RGG/logs/present_cosmo_%A_%a.log
+#SBATCH --error=sasthana/Downscaling/COSMO_RGG/logs/present_cosmo_%A_%a.log
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4       
 #SBATCH --mem=256G
@@ -17,14 +17,14 @@ BASE_DIR="/work/FAC/FGSE/IDYST/tbeucler/downscaling"
 
 cd "$BASE_DIR" || { echo "failed to cd into base dir"; exit 1; }
 
-mkdir -p "$BASE_DIR/sasthana/Downscaling/COSMO_RGG/COSMO/COSMO_pgw"
+mkdir -p "$BASE_DIR/sasthana/Downscaling/COSMO_RGG/COSMO/COSMO_present"
 
 
 cdo sellonlatbox,-10,30,35,70 -selname,T_2M \
-    -cat "/reference/FGSE/climate_simulations/cosmo6_2km_climate_PGW_MPI_HR/1h_2D/*.nz" \
-    "$BASE_DIR/sasthana/Downscaling/COSMO_RGG/COSMO/COSMO_pgw/COSMO_t2m_pgw_europe.nc"
+    -cat "/reference/FGSE/climate_simulations/cosmo6_2km_climate/1h_2D/*.nz" \
+    "$BASE_DIR/sasthana/Downscaling/COSMO_RGG/COSMO/COSMO_present/COSMO_t2m_present_europe.nc"
 
 cdo sellonlatbox,-10,30,35,70 -daysum \
     -selname,TOT_PREC \
-    -cat "/reference/FGSE/climate_simulations/cosmo6_2km_climate_PGW_MPI_HR/5min_2D/*.nz" \
-    "$BASE_DIR/sasthana/Downscaling/COSMO_RGG/COSMO/COSMO_pgw/COSMO_totprec_daily_pgw_europe.nc"
+    -cat "/reference/FGSE/climate_simulations/cosmo6_2km_climate/5min_2D/*.nz" \
+    "$BASE_DIR/sasthana/Downscaling/COSMO_RGG/COSMO/COSMO_present/COSMO_totprec_daily_present_europe.nc"
